cb71ff17d6f7f05ff3e51331c6dcb1d03d805e25
dallli92 <dallli92@live.fr>
2019-06-09 13:51:34 +0200

    adapt and hold

---
+++ app/src/main/java/com/dalilandoulsi/clothesme/Adapters/CatAdapter.kt
@@ -0,0 +1,60 @@
+package com.dalilandoulsi.clothesme.Adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.dalilandoulsi.clothesme.Model.Category
+import com.dalilandoulsi.clothesme.R
+
+class CatAdapter(context: Context, categories: List<Category>) : BaseAdapter() {
+
+    val context = context
+    val categories = categories
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val catView: View
+        val holder: ViewHolder
+
+        if (convertView == null) {
+
+            holder = ViewHolder()
+            catView = LayoutInflater.from(context).inflate(R.layout.cat_list_item, null)
+            holder.catImage = catView.findViewById(R.id.catImg)
+            holder.catName = catView.findViewById(R.id.catName)
+            catView.tag = holder
+            println("firsty")
+        } else {
+            holder = convertView.tag as ViewHolder
+            catView = convertView
+            println("recycle")
+
+        }
+
+        val category = categories[position]
+        holder.catName?.text = category.title
+        val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName)
+        holder.catImage?.setImageResource(resourceId)
+        return catView
+    }
+
+    override fun getItem(position: Int): Any {
+        return categories[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getCount(): Int {
+        return categories.count()
+    }
+
+    private class ViewHolder {
+
+        var catImage: ImageView? = null
+        var catName: TextView? = null
+    }
+}
--- app/src/main/java/com/dalilandoulsi/clothesme/Controller/MainActivity.kt
+++ app/src/main/java/com/dalilandoulsi/clothesme/Controller/MainActivity.kt
@@ -2,12 +2,21 @@ package com.dalilandoulsi.clothesme.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.dalilandoulsi.clothesme.Adapters.CatAdapter
+import com.dalilandoulsi.clothesme.Model.Category
 import com.dalilandoulsi.clothesme.R
+import com.dalilandoulsi.clothesme.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+
+    lateinit var adapter: CatAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        adapter = CatAdapter(this, DataService.categories)
+        CatListView.adapter = adapter
     }
 }
