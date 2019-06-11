28eb8aea8ed88befa6dc1cdeec3af134a159ed09
dallli92 <dallli92@live.fr>
2019-06-11 14:19:59 +0200

    product details

---
+++ app/src/main/java/com/dalilandoulsi/clothesme/Controller/ProductDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.dalilandoulsi.clothesme.Controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_details)
+    }
+}
